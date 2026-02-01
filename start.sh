eff4b05909999537ae068e128b49d3739b811f1e
cwh01024 <cwh01024@gmail.com>
2026-02-01 15:48:52 -0800

    config: add bot token for @anti_worker_777_bot

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Telegram Remote Controller 啟動腳本
+# Bot: @anti_worker_777_bot
+
+export TELEGRAM_BOT_TOKEN="8402311701:AAH9By6xOCvUpBsVbHXAT1J2YjT53Oty4E8"
+
+cd "$(dirname "$0")"
+./telegram-remote-controller
